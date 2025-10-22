2fade547a512c3780fa12cb5bfcae8c6bfb6302d
aahilalwani25 <alwani.aahil25@gmail.com>
2025-10-22 14:02:30 +0500

    fix: authorization fix on active projects api

--- src/hooks/useFreelancerAPI.js
+++ src/hooks/useFreelancerAPI.js
@@ -111,7 +111,8 @@ export const useFreelancerAPI = ({ bidderType, autoBidType }) => {
 
       const response = await axios.get(url, {
         headers: {
-          'Authorization': `Bearer ${token}`,
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+          "Accept": "application/json",
         },
       });
 
